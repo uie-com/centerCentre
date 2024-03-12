244dcf168be320884e9060b16856f2b3944f525c
Anna Gondring <32964281+zednon@users.noreply.github.com>
2024-03-12 15:05:26 -0400

    update to the HTML css

--- src/main.js
+++ src/main.js
@@ -19,3 +19,27 @@ function toggleDropdown() {
       }
     }
   }
+
+  function toggleDropdownProgram() {
+    var dropdownContentProgrram = document.getElementById("myDropdownProgram");
+    if (dropdownContentProgrram.style.display === "block") {
+      dropdownContentProgrram.style.display = "none";
+    } else {
+      dropdownContentProgrram.style.display = "block";
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdownprogram-btn')) {
+     console.log("test");
+      var dropdownPrograms = document.getElementsByClassName("dropdownprogram-content");
+      for (var i = 0; i < dropdownPrograms.length; i++) {
+        var openDropdownProgram = dropdownPrograms[i];
+        if (openDropdownProgram.style.display === "block") {
+          openDropdownProgram.style.display = "none";
+        }
+      }
+    }
+  }
+
